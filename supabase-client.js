0750036abd5789f11e80b8013c859346eece3161
jm182-boop <jmfernandez182@gmail.com>
2026-05-30 09:54:29 -0300

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 // 1. Pega tus claves reales entre las comillas simples
-const supaUrl = 'PEGAR_AQUI_TU_URL';
-const supaKey = 'PEGAR_AQUI_TU_KEY';
+const supaUrl = 'https://aphycznoynnrnipbtyym.supabase.co';
+const supaKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwaHljem5veW5ucm5pcGJ0eXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MDY4MzIsImV4cCI6MjA5NTQ4MjgzMn0.aexuHjw2bvMNquv9FKN9kUsPb9pvIO1uHjM6H7e6OOs';
 
 // 2. Inicializamos la conexión y obligamos a toda la app a reconocerla
 window.supabase = window.supabase.createClient(supaUrl, supaKey);
